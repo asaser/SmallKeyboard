58c67b6599699a98a0c325f592cc04a40731b5e1
asaser <asaser11@gmail.com>
2019-01-25 21:15:50 +0100

    Ready Easy Keyboard

--- js/main.js
+++ js/main.js
@@ -1,18 +1,41 @@
-function playSong(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"`);
-    if(!audio) return;
+//function playSong(e) {
+//    const audio = document.querySelector(`audio[data-key="${e.keyCode}"`);
+//    const key = document.querySelector(`div[data-key="${e.keyCode}"`);
+//    if(!audio) return;
+//
+//    key.classList.add('music');
+//    audio.currentTime = 0;
+//    audio.play();
+//  }
+//  
+//  function removeColoringButton(e) {
+//    if(e.propertyName !== 'transform') return;
+//    e.target.classList.remove('music');
+//  }
+//
+//    const keys = document.querySelectorAll('.key');
+//    keys.forEach(key => key.addEventListener('transitionend', removeColoringButton));
+//    window.addEventListener('keyup', playSong);
 
+
+function playSong(e) {
+    
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+        
     key.classList.add('music');
     audio.currentTime = 0;
     audio.play();
-  }
-  
-  function removeColoringButton(e) {
-    if(e.propertyName !== 'transform') return;
-    e.target.classList.remove('music');
-  }
+}
 
-    const keys = document.querySelectorAll('.key');
-    keys.forEach(key => key.addEventListener('transitionend', removeColoringButton));
-    window.addEventListener('keyup', playSong);
+function removeColorButton(e) {
+    if(e.propertyName !== 'transform') {
+        return;
+    }
+    e.target.classList.remove('music');
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach(key => key.addEventListener("transitionend", removeColorButton));
+window.addEventListener("keyup", playSong);
